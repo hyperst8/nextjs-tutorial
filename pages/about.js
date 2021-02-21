a498c33b2db462fff0694d3426fbc00109694c33
Huy Nguyen <huypn82@gmail.com>
2021-02-21 14:15:05 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur reprehenderit dolorum laudantium, saepe optio modi itaque maiores unde id at minus! Corrupti iste in a soluta veritatis voluptates libero. Voluptate.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum odio consectetur nesciunt dolore voluptas quisquam hic soluta, commodi labore culpa, harum ipsam rerum accusantium obcaecati aliquam nobis, odit ipsa suscipit!</p>
+            </div>
+        </>
+    );
+}
+
+export default About;
